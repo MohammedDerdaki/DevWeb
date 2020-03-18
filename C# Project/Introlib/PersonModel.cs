f761c841b441e3130052e7af040a014c16fc9c9c
MohammedDerdaki <mohammed.derdaki@gmail.com>
2020-03-18 18:03:24 +0100

    C# Frist Project

---
+++ C# Project/Introlib/PersonModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Introlib
+{
+    public class PersonModel
+    {
+        public string FirstName {get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
